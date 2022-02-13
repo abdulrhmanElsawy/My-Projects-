b279a5da8dd198b844c469b97898ea971cc73aa3
Abdelrhman Elsawy <87781842+abdulrhmanElsawy@users.noreply.github.com>
2022-02-13 17:22:05 +0200

    Add files via upload

---
+++ Animated Error Page/js/move.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $(".sun").click(function() {
+        $(".circle").fadeIn(500),
+
+            $(".circle").fadeOut(500),
+            $(".container").toggleClass("stary");
+
+
+    })
+})
